193f5cd32aef35744fa31401da8551cabb40fa52
emi <akiraz@yeah.net>
2026-03-18 21:00:11 +0800

    feat: create auth.handler, handles `auth/register` and `auth/login` requests

---
+++ internal/auth/handler.go
@@ -0,0 +1,58 @@
+package auth
+
+import (
+	"net/http"
+
+	"github.com/aK1r4z/workpal/internal/pkg/response"
+	"github.com/labstack/echo/v5"
+)
+
+type handler struct {
+	authService *service
+}
+
+// 创建认证请求处理器
+func NewHandler(
+	authService *service,
+) *handler {
+	return &handler{
+		authService: authService,
+	}
+}
+
+// 注册路由
+func (h *handler) RegisterRoutes(e *echo.Echo) {
+	g := e.Group("/auth")
+	g.POST("/register", h.register)
+	g.POST("/login", h.login)
+}
+
+// 注册请求处理器
+func (h *handler) register(c *echo.Context) error {
+	req := &registerRequest{}
+	if err := c.Bind(req); err != nil {
+		return response.Fail(c, http.StatusBadRequest, -1, "invalid request")
+	}
+
+	err := h.authService.Register(c.Request().Context(), req.Username, req.Password)
+	if err != nil {
+		return response.Fail(c, http.StatusInternalServerError, -1, err.Error())
+	}
+
+	return response.Success(c, http.StatusCreated, 0, "user created", true)
+}
+
+// 登录请求处理器
+func (h *handler) login(c *echo.Context) error {
+	req := &loginRequest{}
+	if err := c.Bind(req); err != nil {
+		return response.Fail(c, http.StatusBadRequest, -1, "invalid request")
+	}
+
+	ok, err := h.authService.Login(c.Request().Context(), req.Username, req.Password)
+	if err != nil || !ok {
+		return response.Fail(c, http.StatusUnauthorized, -1, "invalid credential")
+	}
+
+	return response.Success(c, http.StatusOK, 0, "login success", true)
+}
