938ae62ab6a23ac2258e707bb33fe80e5af3edbe
emi <akiraz@yeah.net>
2026-03-18 19:10:34 +0800

    refactor: set auth.Service to private. I think this should not be public now?

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -7,21 +7,21 @@ import (
 	"github.com/aK1r4z/workpal/internal/user"
 )
 
-type Service struct {
+type service struct {
 	users user.Store
 }
 
 // 创建用户认证服务
 func NewService(
 	users user.Store,
-) *Service {
-	return &Service {
+) *service {
+	return &service {
 		users: users,
 	}
 }
 
 // 用户注册
-func (s *Service) Register(ctx context.Context, username string, password string) (error) {
+func (s *service) Register(ctx context.Context, username string, password string) (error) {
 	auth, err := GenerateAuth(Config, password)
 	if err != nil {
 		return err
@@ -37,7 +37,7 @@ func (s *Service) Register(ctx context.Context, username string, password string
 	return nil
 }
 
-func (s *Service) Login(ctx context.Context, username string, password string) (bool, error) {
+func (s *service) Login(ctx context.Context, username string, password string) (bool, error) {
 	u, err := s.users.GetByName(ctx, username)
 	if err != nil {
 		return false, err
