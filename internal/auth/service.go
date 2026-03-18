b09b23733bfb3d031808e6789c5049cc761086f2
emi <akiraz@yeah.net>
2026-03-18 20:58:23 +0800

    feat(user.service): add check for invalid usernames or passwords

--- internal/auth/service.go
+++ internal/auth/service.go
@@ -2,10 +2,38 @@ package auth
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"regexp"
 
 	"github.com/aK1r4z/workpal/internal/user"
 )
+
+// Regexp
+var (
+	//  首位字母，后接字母数字下划线，总长度 3-16
+	regexpUsername = regexp.MustCompile(`^[a-zA-Z][a-zA-Z0-9_]{2,15}$`)
+
+	// 可打印 ASCII 字符，总长度 8-64
+	regexpPassword = regexp.MustCompile(`^[ -~]{8,64}$`)
+)
+
+// Error
+var (
+	ErrInvalidUsername = fmt.Errorf("invalid username")
+	ErrInvalidPassword = fmt.Errorf("invalid password")
+)
+
+// 检测用户名和密码是否有效
+func IsValidCredential(username string, password string) error {
+	if !regexpUsername.MatchString(username) {
+		return ErrInvalidUsername
+	}
+	if !regexpPassword.MatchString(password) {
+		return ErrInvalidPassword
+	}
+	return nil
+}
 
 type service struct {
 	users user.Store
@@ -15,13 +43,17 @@ type service struct {
 func NewService(
 	users user.Store,
 ) *service {
-	return &service {
+	return &service{
 		users: users,
 	}
 }
 
 // 用户注册
-func (s *service) Register(ctx context.Context, username string, password string) (error) {
+func (s *service) Register(ctx context.Context, username string, password string) error {
+	if err := IsValidCredential(username, password); err != nil {
+		return err
+	}
+
 	auth, err := GenerateAuth(Config, password)
 	if err != nil {
 		return err
@@ -32,12 +64,16 @@ func (s *service) Register(ctx context.Context, username string, password string
 		return err
 	}
 
-	log.Printf("User %s(%s): Registered", u.Name, u.ID)
+	log.Printf("user %s(%s): registered", u.Name, u.ID)
 
 	return nil
 }
 
 func (s *service) Login(ctx context.Context, username string, password string) (bool, error) {
+	if err := IsValidCredential(username, password); err != nil {
+		return false, err
+	}
+
 	u, err := s.users.GetByName(ctx, username)
 	if err != nil {
 		return false, err
